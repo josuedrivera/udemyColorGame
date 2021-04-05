e9582cfe3f3a0777de91e7a9ed1d30d53b83b53e
Josue Rivera <41710630+josuedrivera@users.noreply.github.com>
2021-04-04 23:52:57 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 console.log("connected");
-//javascript structure patterns will be something to look into soon
 
-// var colors = [
-//     "rgb(255, 0, 0)",
-//     "rgb(255, 255, 0)",
-//     "rgb(0, 255, 0)",
-//     "rgb(0, 255, 255)",
-//     "rgb(0, 0, 255)",
-//     "rgb(255, 0, 255)",
-// ]
-// var colors = generateRandomColors(numSquares);
-// var pickedColor = pickColor();
 var numSquares = 6;
 var colors = [];
 var pickedColor;
