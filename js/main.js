179978447c3bb1fa220c703a9d71366875302b97
Josue Rivera <41710630+josuedrivera@users.noreply.github.com>
2021-05-09 11:27:40 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -180,28 +180,6 @@ resetButton.addEventListener("click", function(){
     reset();
 });
 
-// colorDisplay.textContent = pickedColor;
-
-// for(var i = 0; i < squares.length; i++){
-//     //add initial colors to squares
-//     squares[i].style.backgroundColor = colors[i];
-//     //add click listeners to squares
-//     squares[i].addEventListener("click", function(){
-//         //grab color of clicked square
-//         var clickedColor = this.style.backgroundColor;
-//         //compare color to pickedColor
-//         if(clickedColor === pickedColor){
-//             messageDisplay.textContent = "Correct!";
-//             resetButton.textContent = "Play Again?";
-//             changeColors(clickedColor);
-//             h1.style.backgroundColor = clickedColor;
-//         } else {
-//             this.style.backgroundColor = "#232323";
-//             messageDisplay.textContent = "Try Again"
-//         }
-//     });
-// }
-
 function changeColors(color) {
     //loop through all squares
     for(var i = 0; i < squares.length; i++) {
