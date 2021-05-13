15473ebf658301b2f12e13e6143f9f2c261c9593
Josue Rivera <41710630+josuedrivera@users.noreply.github.com>
2021-05-13 12:19:12 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -91,22 +91,6 @@ function setupSquares() {
 //     reset();
 // }
 
-// for(var i = 0; i < modeButtons.length; i++){
-//     modeButtons[i].addEventListener("click", function(){
-//         modeButtons[0].classList.remove("selected");
-//         modeButtons[1].classList.remove("selected");
-//         this.classList.add("selected");
-//         //ternary operator method
-//         //this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
-//         if(this.textContent === "Easy") {
-//             numSquares = 3;
-//         } else {
-//             numSquares = 6;
-//         }
-//         reset();
-//     });
-// }
-
 function reset() {
     //generate all new colors
     colors = generateRandomColors(numSquares);
